84c8579bd0f84f498d878baa38acbb9557c6fd9a
repo <juan@latamready.com>
2025-10-29 12:32:25 -0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/Portafolio/', // Base URL for GitHub Pages
   root: 'src', // Your source code is in the 'src' directory
   build: {
     outDir: '../docs', // Output to the 'docs' directory
